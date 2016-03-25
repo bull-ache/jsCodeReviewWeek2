3110e609a772ca00131d0dcd5db2e3649c17021f
Blake Scott <blakerscott@gmail.com>
2016-03-25 14:50:47 -0700

    hard code a few sample foods

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from 'angular2/core';
+import { FoodListComponent } from './food-list.component';
 import { Food } from './food.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Food List</h1>
+      <h3 *ngFor="#food of foods" (click)="foodWasSelected(food)">
+        {{ food.name }}
+      </h3>
     </div>
   `
-})
+  })
+
 export class AppComponent {
+  public foods: Food[];
+  constructor(){
+    this.foods = [
+    new Food("Pizza", "Dominoes", 715),
+    new Food("brussel sprouts", "vegetables", 150),
+    new Food("Ice Cream", "Ben and Jerry's", 954),
+    ];
+  }
 
 }
