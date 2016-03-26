9c2dd1f00e11d3760aef5e71abe21329da2a70a0
Blake Scott <blakerscott@gmail.com>
2016-03-25 17:09:16 -0700

    moved all componenets to the app component file and changed console.log output to show description and calories only

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from 'angular2/core';
-import { FoodListComponent } from './food-list.component';
-import { Food } from './food.model';
+// import { FoodListComponent } from './food-list.component';
+// import { Food } from './food.model';
+
+
+@Component({
+  selector: 'food-list',
+  inputs: ['foodList'],
+  template: `
+  <h3 *ngFor="#currentFood of foodList" (click)="foodClicked(currentFood)">
+
+  </h3>
+  `
+})
+export class FoodListComponent {
+  public foodList: Food[];
+  foodClicked(clickedFood: Food): void {
+    console.log(clickedFood);
+  }
+}
+
+
+
 
 @Component({
   selector: 'my-app',
-  directives: [FoodListComponent],
+  // directives: [FoodListComponent],
   template: `
     <div class="container">
       <h1>Food List</h1>
@@ -19,10 +39,18 @@ export class AppComponent {
   public foods: Food[];
   constructor(){
     this.foods = [
-    new Food("Pizza", "Dominoes", 715),
-    new Food("brussel sprouts", "vegetables", 150),
-    new Food("Ice Cream", "Ben and Jerry's", 954),
+      new Food("Pizza", "Dominoes", 715, 0),
+      new Food("brussel sprouts", "vegetables", 150, 1),
+      new Food("Ice Cream", "Ben and Jerry's", 954, 2)
     ];
   }
+  foodWasSelected(clickedFood: Food): void {
+    console.log(clickedFood.description, clickedFood.calories);
+}
+}
 
+export class Food {
+  constructor(public name: string, public description: string, public calories: number, public id: number) {
+
+  }
 }
